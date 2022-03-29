e95579ad24cee0bffe78f85ff8f28fe64a3fc5dc
carlossgabriel <cgsalbuquerque@gmail.com>
2022-03-29 08:53:49 -0300

    added eslint, chakra-ui and started the development of intersection

--- pages/intersection/index.tsx
+++ pages/intersection/index.tsx
@@ -1,7 +1,9 @@
+import { Input } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import styles from "../../styles/Home.module.css";
 import { useState } from "react";
+
 import BackButton from "../../components/backButton";
+import styles from "../../styles/Home.module.css";
 
 const Intersection: NextPage = () => {
   // const [fibArray, setFibArray] = useState<number[]>([]);
@@ -18,15 +20,6 @@ const Intersection: NextPage = () => {
     return sequence;
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // const value = parseInt(e.target.value);
-    // if (value > 20 || value < 0) {
-    //   return;
-    // }
-    setValue(value);
-    // setFibArray(intersection(value));
-  };
-
   return (
     <>
       <div className={styles.main}>
@@ -39,26 +32,24 @@ const Intersection: NextPage = () => {
             Type two words to find the intersection of the letters between them.
           </p>
           <p className={styles.observation}>
-            Minumum letters: 3
-            Maximum letters: 15
+            Minumum letters: 3 Maximum letters: 15
           </p>
-          <input
-            type={"text"}
-            placeholder={"Type the first word"}
-            maxLength={15}
-            value={value}
-            className={styles.input}
-            onChange={(e) => {
-              handleChange(e);
-            }}
-          ></input>
-          {/* <div className={styles.content}>
-            {fibArray.map((i) => (
-              <p key={i} className={styles.result}>
-                {i}
-              </p>
-            ))}
-          </div> */}
+          <div>
+            <Input
+              type={"text"}
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              placeholder={"Type the first word"}
+              maxLength={15}
+            ></Input>
+            <Input
+              type={"text"}
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              placeholder={"Type the first word"}
+              maxLength={15}
+            ></Input>
+          </div>
         </div>
       </div>
     </>
